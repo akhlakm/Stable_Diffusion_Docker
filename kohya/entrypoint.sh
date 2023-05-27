4535e8a243b9678f14c81a7e1f49983130b27b71
akhlakm <me@akhlakm.com>
2023-05-27 14:31:24 -0400

    Disable headless run

--- kohya/entrypoint.sh
+++ kohya/entrypoint.sh
@@ -13,4 +13,4 @@ fi
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/usr/local/cuda/lib64:/usr/lib/x86_64-linux-gnu/"
 
 exec "$@"
-python kohya_gui.py --listen 0.0.0.0 --server_port 7860 --headless
+python kohya_gui.py --listen 0.0.0.0 --server_port 7860
